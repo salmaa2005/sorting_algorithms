82a40f10bae1bae7877f16a63074cbcf55d73253
salmaa2005 <salmaamostafa2005@gmail.com>
2024-02-21 01:47:42 +0200

    Task 2 selection sort v1.0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -42,7 +42,7 @@ void selection_sort(int *array, size_t size)
 			temp = array[i];
 			array[i] = array[min];
 			array[min] = temp;
-			print_array(array, size);
 		}
+		print_array(array, size);
 	}
 }
